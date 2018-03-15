78f1c7cf2b23e0d2a80d5005714b3f161d7344f1
MollyLJohnson <mollyl.johnson@att.net>
2018-03-15 14:22:57 -0500

    trying to fix where the code pulls the name, party, and photo from

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $("form").on("submit", function(e) {
     e.preventDefault();
+    $(".scrolling-profiles").empty();
     var input = $("#address").val();
     if (input.length != 5) {
         // validate user input here!!!
@@ -17,48 +18,58 @@ $("form").on("submit", function(e) {
         method: "GET"
       })
         .then(function(response) {
+        
             for (var k = 0; k < response.offices.length; k++) {
                 var position = response.offices[k].name;
                 // person's official position
-                console.log(position);
-                var name = response.officials[k].name;
+                
+                var indices = [];
+                //loop through the offices.officialIndices?
+                indices = response.offices[k].officialIndices;
+                console.log(indices);
+                
+                var name = response.officials[indices].name;
                 // person's name
-                var party = response.officials[k].party;
+                var party = response.officials[indices].party;
                 // person's political party
-                var photo = response.officials[k].photoUrl;
+                var photo = response.officials[indices].photoUrl;
                 // person's photo src
-                console.log(name +"---"+ party);
-            
         
                 var newOfficial = $("<div class='card profile-card'>");
-                //creates a newOfficial variable with a class profile-card
+                // creates a newOfficial variable with a class profile-card
 
                 var wrapper = $("<div class='imgwrapper' id='"+ name +"' >");
-                //
+                // adds a div with a class imgwrapper and an id with the officials name
 
                 var officialPhoto = $("<img class='card-img-top img-fluid img-responsive profile-img'>").attr("src", photo);
-                //
+                // adds a img with a class card-img-top img-fluid img-responsive profile-img and sets the source to the officials photo from the civic information api
+                
+                var officialBody = $("<div class='card-body text-center'>");
+                // adds a div with a class card-body text-center for formatting
 
                 var officialName = $("<h5 class='card-title'>").text(name);
-                //
+                // adds a h5 with a class card-title with the officials name
 
                 var officialPosition = $("<p class='card-text text-muted'>").text(position + " --- " + party);
-                //
+                // adds a p with a class card-text text-muted with the officials position and party
 
                 var officialSocialDiv = $("<div class='social-links' id='"+ name +"'>").text();//for loop here
-                //
+                // adds a div with a class social-links and an id with the officials name with their specific social media links
 
                 wrapper.append(officialPhoto);
-                //
+                // puts the officialPhoto into the wrapper div
+                
+                officialBody.append(officialName)
+                            .append(officialPosition)
+                            .append(officialSocialDiv);
+                // puts the officialName, officialPosition and the officialSocialDiv into the officialBody div
 
                 newOfficial.append(wrapper)
-                           .append(officialName)
-                           .append(officialPosition)
-                           .append(officialSocialDiv);
-                //adds the new elements to the newOfficial
+                           .append(officialBody);
+                //adds the wrapper with the image and the officialBody to the newOfficial
 
                 $(".scrolling-profiles").append(newOfficial);
-                // dynamically creates new cards with official data
+                // dynamically creates new cards with each official's data
             }
         
         })
