1f34fb4fbe33c89461c689e31076cf05a3998746
MollyLJohnson <mollyl.johnson@att.net>
2018-03-15 13:53:30 -0500

    dynamically built the cards for each candidate

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,17 +18,49 @@ $("form").on("submit", function(e) {
       })
         .then(function(response) {
             for (var k = 0; k < response.offices.length; k++) {
-                    var posistion = response.offices[k].name;
-                    // person's official position
-           console.log(posistion);
-                    var name = response.officials[k].name;
-                    // person's name
-                    var party = response.officials[k].party;
-                    // person's political party
-                    var photo = response.officials[k].photoUrl;
-                    // person's photo src
+                var position = response.offices[k].name;
+                // person's official position
+                console.log(position);
+                var name = response.officials[k].name;
+                // person's name
+                var party = response.officials[k].party;
+                // person's political party
+                var photo = response.officials[k].photoUrl;
+                // person's photo src
                 console.log(name +"---"+ party);
+            
+        
+                var newOfficial = $("<div class='card profile-card'>");
+                //creates a newOfficial variable with a class profile-card
+
+                var wrapper = $("<div class='imgwrapper' id='"+ name +"' >");
+                //
+
+                var officialPhoto = $("<img class='card-img-top img-fluid img-responsive profile-img'>").attr("src", photo);
+                //
+
+                var officialName = $("<h5 class='card-title'>").text(name);
+                //
+
+                var officialPosition = $("<p class='card-text text-muted'>").text(position + " --- " + party);
+                //
+
+                var officialSocialDiv = $("<div class='social-links' id='"+ name +"'>").text();//for loop here
+                //
+
+                wrapper.append(officialPhoto);
+                //
+
+                newOfficial.append(wrapper)
+                           .append(officialName)
+                           .append(officialPosition)
+                           .append(officialSocialDiv);
+                //adds the new elements to the newOfficial
+
+                $(".scrolling-profiles").append(newOfficial);
+                // dynamically creates new cards with official data
             }
+        
         })
     
 })
