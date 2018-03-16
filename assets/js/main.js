fd779111c7d29dc0883d03bc94ab23301daafe5e
MollyLJohnson <mollyl.johnson@att.net>
2018-03-16 18:05:29 -0500

    checked if the officials had a photo url and if not I put a placeholder image in for their card

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,14 @@ $("form").on("submit", function(e) {
         method: "GET"
       })
         .then(function(response) {
-        console.log(response);
+        console.log(response.officials[4].photoUrl);
             for (var k = 0; k < response.offices.length; k++) {
                 
                 var position = response.offices[k].name;
                 // person's official position
                 
                 var officialInd = [];
+                // create an empty array
                 
                 for(var m = 0; m < response.offices[k].officialIndices.length; m++) {
                     
@@ -32,55 +33,62 @@ $("form").on("submit", function(e) {
                         {position: position, 
                         index: response.offices[k].officialIndices[m]}
                     );   
+                    // pushes an object into the empty array
                 }
-//                console.log(officialInd);
 
-                for (var b = 0; b < officialInd.length; b++){
+                for (var b = 0; b < officialInd.length; b++) {
                     var indexofName = officialInd[b].index;
-                    
+                    // get's the associated index value
                     var name = response.officials[indexofName].name;
                     // person's name
-                    console.log(name);
                     var party = response.officials[indexofName].party;
                     // person's political party
-                    var photo = response.officials[indexofName].photoUrl;
-                    // person's photo src
+                    
+                    if (!response.officials[indexofName].photoUrl) { 
+                        var photo = "../../assets/images/psicon.png";
+                        // sets the picture url to one of our pictures
+                        var officialPhoto = $("<img class='card-img-top img-fluid img-responsive profile-img'>").attr("src", photo);
+                        // adds a img with a class card-img-top img-fluid img-responsive profile-img and sets the source to the officials photo from the civic information api
+                    }
+                    else {
+                        var photo = response.officials[indexofName].photoUrl;
+                        // person's photo src
+                        var officialPhoto = $("<img class='card-img-top img-fluid img-responsive profile-img'>").attr("src", photo);
+                        // adds a img with a class card-img-top img-fluid img-responsive profile-img and sets the source to the officials photo from the civic information api
+                    }
                 
-                var newOfficial = $("<div class='card profile-card'>");
-                // creates a newOfficial variable with a class profile-card
+                    var newOfficial = $("<div class='card profile-card'>");
+                    // creates a newOfficial variable with a class profile-card
 
-                var wrapper = $("<div class='imgwrapper' id='"+ name +"' >");
-                // adds a div with a class imgwrapper and an id with the officials name
+                    var wrapper = $("<div class='imgwrapper' id='"+ name +"' >");
+                    // adds a div with a class imgwrapper and an id with the officials name
 
-                var officialPhoto = $("<img class='card-img-top img-fluid img-responsive profile-img'>").attr("src", photo);
-                // adds a img with a class card-img-top img-fluid img-responsive profile-img and sets the source to the officials photo from the civic information api
-                
-                var officialBody = $("<div class='card-body text-center'>");
-                // adds a div with a class card-body text-center for formatting
+                    var officialBody = $("<div class='card-body text-center'>");
+                    // adds a div with a class card-body text-center for formatting
 
-                var officialName = $("<h5 class='card-title'>").text(name);
-                // adds a h5 with a class card-title with the officials name
+                    var officialName = $("<h5 class='card-title'>").text(name);
+                    // adds a h5 with a class card-title with the officials name
 
-                var officialPosition = $("<p class='card-text text-muted'>").text(position + " --- " + party);
-                // adds a p with a class card-text text-muted with the officials position and party
+                    var officialPosition = $("<p class='card-text text-muted'>").text(position + " --- " + party);
+                    // adds a p with a class card-text text-muted with the officials position and party
 
-                var officialSocialDiv = $("<div class='social-links' id='"+ name +"'>").text();//for loop here
-                // adds a div with a class social-links and an id with the officials name with their specific social media links
+                    var officialSocialDiv = $("<div class='social-links' id='"+ name +"'>").text();//for loop here
+                    // adds a div with a class social-links and an id with the officials name with their specific social media links
 
-                wrapper.append(officialPhoto);
-                // puts the officialPhoto into the wrapper div
-                
-                officialBody.append(officialName)
-                            .append(officialPosition)
-                            .append(officialSocialDiv);
-                // puts the officialName, officialPosition and the officialSocialDiv into the officialBody div
+                    wrapper.append(officialPhoto);
+                    // puts the officialPhoto into the wrapper div
 
-                newOfficial.append(wrapper)
-                           .append(officialBody);
-                //adds the wrapper with the image and the officialBody to the newOfficial
+                    officialBody.append(officialName)
+                                .append(officialPosition)
+                                .append(officialSocialDiv);
+                    // puts the officialName, officialPosition and the officialSocialDiv into the officialBody div
 
-                $(".scrolling-profiles").append(newOfficial);
-                // dynamically creates new cards with each official's data
+                    newOfficial.append(wrapper)
+                               .append(officialBody);
+                    //adds the wrapper with the image and the officialBody to the newOfficial
+
+                    $(".scrolling-profiles").append(newOfficial);
+                    // dynamically creates new cards with each official's data
                 }
                 
                 }
